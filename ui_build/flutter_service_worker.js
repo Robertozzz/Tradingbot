746b9dee8e2b5dec598cb78bfe13ead674462211
unknown <rlmjpluijmen@gmail.com>
2025-08-24 23:46:22 +0300

    Bugfixes

--- ui_build/flutter_service_worker.js
+++ ui_build/flutter_service_worker.js
@@ -24,7 +24,7 @@ const RESOURCES = {"assets/AssetManifest.bin": "0b0a3415aad49b6e9bf965ff578614f9
 "canvaskit/skwasm_heavy.wasm": "8034ad26ba2485dab2fd49bdd786837b",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "flutter.js": "888483df48293866f9f41d3d9274a779",
-"flutter_bootstrap.js": "c99ce67ece4c0d6e7e5a8cdf2c81091c",
+"flutter_bootstrap.js": "d2af208c9ca8d654cc27170532aa0f9b",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
