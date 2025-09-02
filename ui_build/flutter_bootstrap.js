586825c38e454f4d87b2406974ee224d8f7d4302
unknown <rlmjpluijmen@gmail.com>
2025-09-01 20:51:51 -0400

    Multi fix

--- ui_build/flutter_bootstrap.js
+++ ui_build/flutter_bootstrap.js
@@ -38,6 +38,6 @@ _flutter.buildConfig = {"engineRevision":"1e9a811bf8e70466596bcf0ea3a8b5adb5f17f
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "3369814924"
+    serviceWorkerVersion: "984271014"
   }
 });
